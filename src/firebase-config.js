cc6618ec9f515be91e2b3508f99ee31ae4c0b08d
Ahmed <amsos4118@gmail.com>
2024-04-30 07:17:30 +0300

    update firebase-config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,19 @@
-// Import necessary functions from Firebase SDK
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 // Firebase configuration object containing API keys and project details
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCEqfAWg7CsqxLLk5DB7p_NM6hlV5XU9pg",
+  authDomain: "munches-recipe-finder.firebaseapp.com",
+  projectId: "munches-recipe-finder",
+  storageBucket: "munches-recipe-finder.appspot.com",
+  messagingSenderId: "444263619489",
+  appId: "1:444263619489:web:288d66047526e7af8089e0",
 };
 
 // Initialize Firebase app with the provided configuration
 const app = initializeApp(firebaseConfig);
-
 // Get the authentication service instance from Firebase app
 const auth = getAuth(app);
-
 // Export the initialized Firebase app and authentication service instance
-export { app, auth };
+export {app , auth};
