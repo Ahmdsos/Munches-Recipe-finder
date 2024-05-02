d70ef94fe7394344a6c31a16e70325d88ce1bb5c
Ahmed <amsos4118@gmail.com>
2024-05-03 02:16:47 +0300

    update search

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -129,7 +129,7 @@ const Red = styled.h2`
 `;
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: 2rem;
 `;
 
